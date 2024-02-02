63805ade9fbf5e8e8cc47df4872229b34e2364a5
Ramesh <rameshramesh4426@gmail.com>
2024-02-02 14:49:11 +0530

    hello

---
+++ Homework/React/hello/src/pages/Dashboard.js
@@ -0,0 +1,86 @@
+
+
+function Dashboard(){
+    return( <div className="parent">
+        <div className="top">
+ramesh
+        </div>
+        <div className="bottom">
+            <div className="left">raja</div>
+            <div className="right">
+            <h2>Student Mark Sheet</h2>
+            <center>
+            <table border = "1">
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Age</th>
+                    <th>Mark1</th>
+                    <th>Mark2</th>
+                    <th>Mark2</th>
+                    <th>Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Ramesh</td>
+                    <td>male</td>
+                    <td>22</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>270</td>
+                    </tr>
+
+                    <tr>
+                    <td>Ramesh</td>
+                    <td>male</td>
+                    <td>22</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>270</td>
+                    </tr>
+
+                    <tr>
+                    <td>Ramesh</td>
+                    <td>male</td>
+                    <td>22</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>270</td>
+                    </tr>
+
+                    <tr>
+                    <td>Ramesh</td>
+                    <td>male</td>
+                    <td>22</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>270</td>
+                    </tr>
+
+                    <tr>
+                    <td>Ramesh</td>
+                    <td>male</td>
+                    <td>22</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>90</td>
+                    <td>270</td>
+                    </tr>
+                </tbody>
+               
+            </table>
+            </center>
+            </div>
+            
+        </div>
+
+    </div>
+    );
+}
+export default Dashboard;
