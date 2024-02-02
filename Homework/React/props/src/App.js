2613d842d7b4842e6d5c27d6a42c1a1254fb40ae
Ramesh <rameshramesh4426@gmail.com>
2024-02-02 14:47:11 +0530

    props

---
+++ Homework/React/props/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './component/Props';
+
+function App() {
+  return (
+    <div className="App">
+     <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ Homework/React/props/src/component/Props.js
@@ -0,0 +1,32 @@
+// import React from "react";
+
+// class props extends React.Component{
+//     constructor(props){
+//         super();
+//     }
+// } 
+
+function Hello (){
+    // let a = 10;
+    // let b = 20;
+    // const add = () =>{
+    //     return a+b
+    // }
+    // return(<div className = "parent">
+       
+    //   {a}+{b} = {add()}
+       
+    // </div>
+
+    // );
+    return (<div className = "parent">
+      <div className = "header"></div>
+      <div className = "bottom">
+        <div className = "left"></div>
+        <div className = "right"></div>
+      </div>
+    </div>
+
+    );
+}
+export default Hello;
